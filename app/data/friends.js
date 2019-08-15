54919dcb680f69310fe07130507fcc2aa25165ad
nick <lombardo15@temple.edu>
2019-08-15 16:25:55 -0400

    first

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+const friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,5,2,3,3,5,2,2,2,3]
+    },
+    {
+        "name":"Nick",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[1,4,4,1,2,5,1,3,4,4]
+
+    },
+    {
+        "name":"Jason",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,1,2,2,3,3,5,2,1,3]
+    },
+    {
+        "name":"Dan",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[4,2,2,3,4,5,3,5,2,4]
+    },
+    {
+        "name":"Bob",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[3,1,3,2,3,4,2,1,2,2]
+    }
+]
+
+
+module.exports= friends;
