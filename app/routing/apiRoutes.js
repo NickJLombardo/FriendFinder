9ec392162011140f8f8616fab64d6c92796d7b8e
nick <lombardo15@temple.edu>
2019-08-16 16:02:55 -0400

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require("../data/friends"); 
+var friends = require("../data/friends.js"); 
 
 module.exports = function(app) {
 
@@ -7,31 +7,28 @@ module.exports = function(app) {
     })
  
     app.post("/api/friends", function(req, res){
-      console.log(req.body.scores);
+      console.log(req.body);
   
       var userInput = req.body;
   
       var userResponses = userInput.scores;
       var matchName = '';
-      var matchImage = '';
       var totalDifference = 1000; 
-
+         
       for (var i = 0; i < friends.length; i++) {
   
-       var diff = 0;
+      var diff = 0;
        for (var j = 0; j < userResponses.length; j++) {
-          diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+         diff = Math.abs(friends[i].scores[j]-userResponses[j]);
          }
          if (diff < totalDifference) {
-  
             totalDifference = diff;
-            matchName = friends[i].name;
-            matchImage = friends[i].photo;
+            matchName = friends[i].name; 
          }
       }
       friends.push(userInput);
 
-      res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+      res.json({status: 'OK', matchName: matchName});
    });
 
 
