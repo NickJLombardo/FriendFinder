0e10c2d9197ef9215e3f3ffa080e1f51d722ab14
nick <lombardo15@temple.edu>
2019-08-16 16:06:14 -0400

    final

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
